9c6e0a3b9c54cba141109ad872584aca38251991
Abdullah <mohammadabdullahfarooqui27@gmail.com>
2026-04-27 23:58:37 +0500

    some overflow issues solved

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const homeBtn = document.getElementById("home-btn");
 const info = document.querySelector(".info");
 
 function hideIntro() {
+	window.scrollTo({ behavior:'smooth',left: 0, top: 0 });
 	canvas.classList.add("visible");
 	intro.classList.remove("visible");
 	homeBtn.classList.add("visible");
