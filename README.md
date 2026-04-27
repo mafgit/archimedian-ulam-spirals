66744799b0c2ca8b0e50d07c08555f4aa2003b00
Abdullah <mohammadabdullahfarooqui27@gmail.com>
2026-04-27 23:46:51 +0500

    added two modes inside archimedian.js (not UI) updated readme

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# Archimedian & Ulam Spirals
+# Archimedian & Ulam Spiral Animations
 
-- Using JavaScript Canvas API
+- Using Vanilla HTML, CSS, JavaScript & JavaScript Canvas API
+
+
+### Intro
+<img src="./images/intro.png" style="border-radius:10px"/>
+
+### Archimedian Spiral
+<img src="./images/archimedian.png" style="border-radius:10px"/>
+
+### Ulam Spiral
+<img src="./images/ulam.png" style="border-radius:10px"/>
--- archimedian.js
+++ archimedian.js
@@ -1,30 +1,47 @@
-function archimedian(onlyPrimes = false, maxPoints = 1000) {
+function archimedian(
+	showOnlyPrimes = false,
+	dontGivePrimesSeparateColor = false,
+	maxPoints = 1000,
+) {
+	if (showOnlyPrimes && dontGivePrimesSeparateColor)
+		throw new Error("Both modes can't be true at the same time");
+
 	resetCanvas();
 
-	const b = 3;
+	function chooseColorFromOptions(r) {
+		const rem = r % 5;
+		if (rem === 0) return "#84edff";
+		else if (rem === 1) return "#fffc5f";
+		else if (rem === 2) return "#8cff5f";
+		else if (rem === 3) return "#ff5ff2";
+		else if (rem === 4) return "#ff6a71";
+		else return "black";
+	}
 
-	function drawPoint(r, t, prevCoords = null) {
+	let b = 3;
+
+	function draw(x, y) {
+		ctx.beginPath();
+		ctx.roundRect(x, y, b, b, [20]);
+		ctx.fill();
+	}
+
+	function drawPoint(r, t) {
+		let prime = isPrime(r);
+
+		if (showOnlyPrimes && !prime) return;
+
 		let x = cx + r * Math.cos(t) - b / 2;
 		let y = cy - (r * Math.sin(t) - b / 2);
 
-		let prime = isPrime(r);
-
-		if (prime) ctx.fillStyle = "white";
-		else {
-			if (!onlyPrimes) {
-				const rem = r % 3;
-				if (rem === 0) ctx.fillStyle = "#84edff";
-				else if (rem === 1) ctx.fillStyle = "#ff6a71";
-				else if (rem === 2) ctx.fillStyle = "#8cff5f";
-				else ctx.fillStyle = "black";
-			}
+		if (dontGivePrimesSeparateColor) {
+			ctx.fillStyle = chooseColorFromOptions(r);
+		} else {
+			if (prime) ctx.fillStyle = "white";
+			else ctx.fillStyle = chooseColorFromOptions(r);
 		}
 
-		if (!onlyPrimes || prime) {
-			ctx.beginPath();
-			ctx.roundRect(x, y, b + 1, b + 1, [20]); // + 1 to fix unwanted spaces between rows
-			ctx.fill();
-		}
+		draw(x, y);
 
 		// if (r <= 1) {
 		// 	ctx.textAlign = "center";
@@ -33,8 +50,6 @@ function archimedian(onlyPrimes = false, maxPoints = 1000) {
 		// 	ctx.fillStyle = "#ffffff";
 		// 	ctx.fillText(`(${r},${t})`, x, y);
 		// }
-
-		return { x, y };
 	}
 
 	// function drawAxes() {
@@ -44,10 +59,9 @@ function archimedian(onlyPrimes = false, maxPoints = 1000) {
 	// }
 
 	let i = 1;
-	let prevCoords = null;
 
 	function frame() {
-		prevCoords = drawPoint(i, i, prevCoords);
+		drawPoint(i, i);
 
 		i++;
 		if (i <= maxPoints) if (!animStopped) setTimeout(frame, 30);
